98cb9c9269f34e5b17da5eced3dd6f21a0b12a83
SumitNITS321 <submit.9sh@gmail.com>
2025-11-23 15:10:38 +0530

    Add project from MinGW folder

---
+++ CS50/hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+    int count = 1;
+    while(count>0){
+        printf("%d ",count);
+        count++;
+    } 
+    printf("Hello World!");
+}
+//up key just paste's the last command given in terminal
+//up key two times gives second last command
---
+++ CS50/tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    long x;
+    long y;
+    printf("Enter x: ");
+    scanf("%ld",&x);
+    printf("Enter y: ");
+    scanf("%ld",&y);
+
+    double z = (double)x/(double)y;//type casting
+    printf("%.20f",z);
+
+}
